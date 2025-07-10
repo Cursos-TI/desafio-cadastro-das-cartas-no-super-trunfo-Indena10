99967e5269f8ee465bacfa3aeb02361bd8a834dc
Indena10 <carlos.indena@gmail.com>
2025-07-10 18:31:40 +0000

    fazendo as saidas apaecerem

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-int populacao, NPontos;
+int populacao, NPontos, estado[20];
 float PIB, area;
-char nomeCidade[50], codigo[2], estado[1];
+char nomeCidade[50],codigo;
 
-printf("CARTA 1\n");
+
    
 printf("Digite o Estado entre A a H: \n");
-scanf("%c", &estado);
+scanf("%s", &estado);
 
 printf("Digite o código entre 01 e 04: \n");
 scanf("%s", &codigo);
@@ -28,5 +28,12 @@ scanf("%f", &PIB);
 printf("Digite a quantidade de pontos turísticos existentes: \n");
 scanf("%d", &NPontos);
 
-    return 0;
+printf("CARTA 1\n");
+printf("Estado:%s \n",estado);
+printf("Código: \n");
+printf("Nome do Cidade: \n");
+printf("População: \n");
+printf("PIB: \n");
+printf("Numero de pontos turísticos: \n");
+return 0;
 }
